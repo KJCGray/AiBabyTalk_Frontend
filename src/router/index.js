431ae9c9dc6fa60fb74b70b7543d032fc9c60ed6
YvonneJin1826 <yvonne.jin@hixcare.tw>
2024-08-12 14:41:28 +0800

    feat/登入頁面、openai
    1. 新增首頁路徑
    2. 新增登入頁面
    3. pip install openai (python函式庫)

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,18 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginAndRegist from '@/views/LoginAndRegist.vue';
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/login',
+    name: 'login',
+    component: LoginAndRegist
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
---
+++ src/views/LoginAndRegist.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+div(class="w-screen h-screen bg-[#FFFDD0] flex justify-center items-center")
+  div(class="w-[50%] h-[75%] bg-white rounded flex flex-col items-center justify-center")
+    div(class="w-[70%] h-9 rounded flex justify-around shadow-lg mt-4")
+      button(class="w-[50%] rounded-l h-full bg-[#FFA500]") 登入
+      button(class="w-[50%] rounded-r hover:bg-[#ffa600d6]") 註冊
+    div(class="w-[75%] h-[80%] flex flex-col justify-around items-center")
+      div(class="w-full flex justify-center items-center mr-6")
+        p 用戶名稱
+        input(class="w-[60%] h-9 ml-4 p-1 border rounded focus:outline-none")
+      div(class="w-full flex justify-center items-center")
+        p 密碼
+        input(class="w-[60%] h-9 ml-6 p-1 border rounded focus:outline-none")
+      div(class="w-full flex justify-around items-center")
+        div(class="flex items-center")
+          input(type="radio" class="w-4 h-4 mr-1.5" name="identity" id="parent" checked)
+          label(for="parent" class="cursor-pointer select-none") 我是家長
+        div(class="flex items-center")
+          input(type="radio" class="w-4 h-4 mr-1.5" name="identity" id="child")
+          label(for="child" class="cursor-pointer select-none") 我是兒童
+      button(class="w-[40%] h-9 bg-[#FFA500] rounded hover:bg-[#ffa600d6]") 登入
+</template>
