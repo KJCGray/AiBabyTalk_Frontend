eaf01e593da00666e70324adb83bb58587128771
李青芳 <a0905001923@gmail.com>
2024-09-27 10:40:41 +0800

    mainpage以及功能頁導航

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,42 @@
 // src/router/index.js
-import { createRouter, createWebHistory } from 'vue-router';
-import LoginAndRegist from '@/views/LoginAndRegist.vue';
-import MainPage from '@/views/MainPage.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import LoginAndRegist from "@/views/LoginAndRegist.vue";
+import MainPage from "@/views/MainPage.vue";
+import ChatBot from "@/views/ChatBot.vue";
+import Game from "@/views/Game.vue";
+import LearnBot from "@/views/LearnBot.vue";
 
 const routes = [
   {
-    path: '/login',
-    name: 'login',
-    component: LoginAndRegist
+    path: "/login",
+    name: "login",
+    component: LoginAndRegist,
   },
   {
-    path: '/',
-    name: 'main',
-    component: MainPage
+    path: "/",
+    name: "main",
+    component: MainPage,
+  },
+  {
+    path: "/chat",
+    name: "chatbot",
+    component: ChatBot,
+  },
+  {
+    path: "/game",
+    name: "game",
+    component: Game,
+  },
+  {
+    path: "/learn",
+    name: "learnbot",
+    component: LearnBot,
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 });
 
 export default router;
